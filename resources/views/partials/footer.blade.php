87c53f2cad9240d483a9cce48eaf4bc59957e752
chrisszeluga <chris.szeluga@yahoo.com>
2019-03-19 23:06:25 -0400

    adjust copyright text

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md address">2400 Arcola Ave, Wheaton, Maryland 20902
       </div>
-      <div class="col-md text-md-right">&copy; 2019 Copyright Wheaton Volunteer Rescue Squad
+      <div class="col-md text-md-right">&copy; 2019 Wheaton Volunteer Rescue Squad
       </div>
     </div>
   </div>
