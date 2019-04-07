a03842b27c5e1815d3741860cea843e2fd32c7a3
chrisszeluga <chris.szeluga@yahoo.com>
2019-04-07 12:11:49 -0400

    Add JSON LD for Organization

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -19,4 +19,31 @@
   @endwhile
 
   {!! get_the_posts_navigation() !!}
+
+  <script type="application/ld+json">
+   { "@context": "http://schema.org",
+   "@type": "Organization",
+   "name": "Wheaton Volunteer Rescue Squad",
+   "legalName" : "Wheaton Volunteer Rescue Squad, Inc.",
+   "url": "http://www.wvrs.org",
+   "logo": "https://wvrs.org/wp-content/themes/wvrs-custom/dist/images/icon_bd19bfa7.png",
+   "foundingDate": "1955",
+   "address": {
+   "@type": "PostalAddress",
+   "streetAddress": "2400 Arcola Ave",
+   "addressLocality": "Wheaton",
+   "addressRegion": "MD",
+   "postalCode": "20902",
+   "addressCountry": "USA"
+   },
+   "contactPoint": {
+   "@type": "ContactPoint",
+   "contactType": "Administrative",
+   "telephone": "[+301-949-9673]"
+   },
+   "sameAs": [
+   "https://www.facebook.com/WheatonRescue2/",
+   "https://twitter.com/wheatonrescue2"
+   ]}
+  </script>
 @endsection
