1e1c6544c59c2b301376dc6ef5a2ad735649d138
chrisszeluga <chris.szeluga@yahoo.com>
2019-04-07 09:21:38 -0400

    Add lightweight click to enlarge

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
+// Pluhins
+import baguetteBox from 'baguettebox.js';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Click to enlarge
+    baguetteBox.run('.wp-block-gallery');
+    baguetteBox.run('.wp-block-image');
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
