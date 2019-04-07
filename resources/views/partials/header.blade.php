7c23b4c8307161407573807ae575810fc5773ef5
chrisszeluga <chris.szeluga@yahoo.com>
2019-04-07 15:54:27 -0400

    Fix navbar collapse button

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark static-top">
   <div class="container">
     <a class="navbar-brand" href="{{ home_url('/') }}"><img src="@asset('images/logo.png')" alt="{{ get_bloginfo('name', 'display') }}" /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu( array(
         'theme_location'  => 'primary_navigation',
