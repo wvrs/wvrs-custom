b5902bcc61c1ed124b61b9873f59b857a1e68dec
chrisszeluga <chris.szeluga@yahoo.com>
2019-05-18 22:45:05 -0400

    Add footer icon tooltips

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,9 +3,9 @@
     <div class="row">
       <div class="col-md">
         <ul class="social">
-          <li><a href="https://www.facebook.com/WheatonRescue2/" target="_blank"><img src="@asset('images/social_facebook.svg')"></a></li>
-          <li><a href="https://twitter.com/wheatonrescue2" target="_blank"><img src="@asset('images/social_twitter.svg')"></a></li>
-          <li><a href="https://www.instagram.com/wheatonrescue2" target="_blank"><img src="@asset('images/social_instagram.svg')"></a></li>
+          <li><a href="https://www.facebook.com/WheatonRescue2/" target="_blank" title="View Facebook Page"><img src="@asset('images/social_facebook.svg')"></a></li>
+          <li><a href="https://twitter.com/wheatonrescue2" target="_blank" title="View Twitter Page"><img src="@asset('images/social_twitter.svg')"></a></li>
+          <li><a href="https://www.instagram.com/wheatonrescue2" target="_blank" title="View Instagram Page"><img src="@asset('images/social_instagram.svg')"></a></li>
         </ul>
       </div>
       <div class="col-md text-md-right">&copy; 2019 Wheaton Volunteer Rescue Squad
