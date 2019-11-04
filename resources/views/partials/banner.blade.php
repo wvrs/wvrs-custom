f32d06098c8f1008194395897bcc73798254a563
chrisszeluga <chris.szeluga@yahoo.com>
2019-11-04 15:55:00 -0500

    update banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,13 +1,13 @@
-@if (time() >= 1572400800 && time() <= 1573477200)
+@if (time() >= 1573477200 && time() <= 1577239200)
+  <div class="top-banner">
+    <div class="container">
+      <a href="https://wvrs.org/trees" target="_blank">🎄Christmas tree sales start November 30th! Click for details. 🎄</a>
+    </div>
+  </div>
+@elseif (time() >= 1572400800 && time() <= 1573477200)
   <div class="top-banner">
     <div class="container">
       <a href="https://alumni.wvrs.org/events/2019-shrimp-and-oyster" target="_blank">Buy Tickets Now to the Wheaton Shrimp & Oyster Feast, November 10th 1pm-5pm</a>
-    </div>
-  </div>
-@elseif (time() <= 1572400740)
-  <div class="top-banner box-red">
-    <div class="container">
-      <a href="https://docs.google.com/document/d/1Ne6R0YTsAW_GClMO-a6pbGd5j8zEvHiezdC3rq2YoRM/edit?ts=5daa6ec6#heading=h.gjdgxs" target="_blank">WVRS Statement Regarding the Investigation of Mr. Jacob DeGlopper</a>
     </div>
   </div>
 @endif
