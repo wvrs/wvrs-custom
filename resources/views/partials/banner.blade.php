4506368f99125544752e580defcf0624d41737e2
chrisszeluga <chris.szeluga@yahoo.com>
2020-07-09 20:05:03 -0400

    update covid banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,13 +1,13 @@
-@if (time() >= 1573477200 && time() <= 1577239200)
-  <div class="top-banner">
+{{-- @if (time() >= 1573477200 && time() <= 1577239200) --}}
+  <div class="top-banner box-red">
     <div class="container">
-      <a href="https://wvrs.org/trees" target="_blank">🎄Christmas tree sales start November 30th! Click for details. 🎄</a>
+      <a href="https://docs.google.com/document/d/1SjtLlHRGI-pzlvB-vrCd_pRMyb5aO9xkkoOomsbCiek/edit" target="_blank">WVRS will be postponing all public events hosted inside our building and taking other actions during the COVID-19 pandemic. Read more →</a>
     </div>
   </div>
-@elseif (time() >= 1572400800 && time() <= 1573477200)
+{{-- @elseif (time() >= 1572400800 && time() <= 1573477200)
   <div class="top-banner">
     <div class="container">
       <a href="https://alumni.wvrs.org/events/2019-shrimp-and-oyster" target="_blank">Buy Tickets Now to the Wheaton Shrimp & Oyster Feast, November 10th 1pm-5pm</a>
     </div>
   </div>
-@endif
+@endif --}}
